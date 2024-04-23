5fec9a1da52a0a318d2d4f7143e750882e71ff04
Tomas Vomacka <tomasv@iconics.com>
2024-04-23 10:31:27 +0200

    R!! ccw orientation implementation

--- TDD/ConvexHull/ConvexHull.cs
+++ TDD/ConvexHull/ConvexHull.cs
@@ -6,7 +6,7 @@ namespace ConvexHull
     {
         public static bool OrientedClockwise(PointF p1, PointF p2, PointF p3)
         {
-            return false;
+            return p1.X * p2.Y + p3.X * p1.Y + p2.X * p3.Y - p3.X * p2.Y - p2.X * p1.Y - p1.X * p3.Y < 0;
         }
     }
 }
