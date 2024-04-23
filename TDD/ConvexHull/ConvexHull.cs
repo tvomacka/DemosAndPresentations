ca38401c9c82443bcd27c666fec1ec3acc3b0e0d
Tomas Vomacka <tomasv@iconics.com>
2024-04-23 10:27:11 +0200

    R!! point orientation failing tests

--- TDD/ConvexHull/ConvexHull.cs
+++ TDD/ConvexHull/ConvexHull.cs
@@ -1,7 +1,12 @@
-﻿namespace ConvexHull
-{
-    public class Class1
-    {
+﻿using System.Drawing;
 
+namespace ConvexHull
+{
+    public class ConvexHull
+    {
+        public static bool OrientedClockwise(PointF p1, PointF p2, PointF p3)
+        {
+            return false;
+        }
     }
 }
