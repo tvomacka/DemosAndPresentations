683ff37fe3124f67517e761b14a20622958e4220
vomicz <t.vomacka@seznam.cz>
2024-04-29 12:27:46 -0700

    RefactorMe project added

---
+++ TDD/RefactorMe/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace RefactorMe
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
